8838322830f2878ccb7c22e2da3f020d6acaea28
Swagatika-dalai <83580248+Swagatika-dalai@users.noreply.github.com>
2022-12-13 12:25:25 +0530

    Add files via upload

---
+++ main.tf.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+    region = var.region
+  
+}
+
+resource "aws_s3_bucket" "poc4_bktt_s3" {
+  count = var.region == "eu-central-1" ? 1 : 0
+  bucket = "hsghgsho135fgshh"
+}
+
+resource "aws_s3_bucket" "poc874_bktt_s3" {
+  count = var.region == "us-east-1" ? 1 : 0
+  bucket = "afvjjoiyr53ch990gv"
+}
+
+
+variable "region" {
+
+}
